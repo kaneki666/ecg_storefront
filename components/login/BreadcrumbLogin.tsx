a547f9b92310684c35761f5585a5dc6ccc1a63c2
Fiurmi <90146718+Fiurmi@users.noreply.github.com>
2022-02-03 01:29:58 +0600

    signin/signup page added

---
+++ components/login/BreadcrumbLogin.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BreadcrumbLogin = () => {
+  return (<div>
+     <nav className="breadcrumb-nav">
+        <div className="container">
+            <ul className="breadcrumb">
+                <li><a href="demo1.html">Home</a></li>
+                <li>My account</li>
+            </ul>
+        </div>
+    </nav> 
+  </div>  
+  );
+};
+
+export default BreadcrumbLogin;
---
+++ components/login/PageHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PageHeader = () => {
+  return (
+    <div>
+        <div className="page-header">
+            <div className="container">
+                <h1 className="page-title mb-0">My Account</h1>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default PageHeader;
