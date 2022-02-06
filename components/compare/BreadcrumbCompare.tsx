caa99dc98f9ca9dfb538bc9acd4f83ab8203bd66
Fiurmi <90146718+Fiurmi@users.noreply.github.com>
2022-02-06 12:23:12 +0600

    compare and wishlist page added

---
+++ components/compare/BreadcrumbCompare.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BreadcrumbCompare = () => {
+  return (<div>
+     <nav className="breadcrumb-nav mb-2">
+        <div className="container">
+            <ul className="breadcrumb">
+                <li><a href="demo1.html">Home</a></li>
+                <li>Compare</li>
+            </ul>
+        </div>
+    </nav>
+  </div>  
+  );
+};
+
+export default BreadcrumbCompare;
---
+++ components/wishlist/BreadcrumbWishlist.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BreadcrumbWishlist = () => {
+  return (<div>
+     <nav className="breadcrumb-nav mb-10">
+        <div className="container">
+            <ul className="breadcrumb">
+                <li><a href="demo1.html">Home</a></li>
+                <li>Wishlist</li>
+            </ul>
+        </div>
+    </nav>
+  </div>  
+  );
+};
+
+export default BreadcrumbWishlist;
