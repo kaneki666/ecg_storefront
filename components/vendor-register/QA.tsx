d3652f4dccb32f8bfa40bc192c12042263abe24d
Fiurmi <90146718+Fiurmi@users.noreply.github.com>
2022-02-17 12:21:03 +0600

    added become a vendor page

---
+++ components/vendor-register/QA.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const QA = () => {
+  return (
+    <div>
+        <div className="container questions mt-2 mt-lg-10 mb-5 mb-lg-9">
+            <h4 className="text-center text-primary font-weight-bold text-capitalize ls-25">Frequently Asked Questions</h4>
+            <h2 className="title title-center">Find an answer to your Question</h2>
+            <div className="row">
+                <div className="col-md-6">
+                    <h4 className="font-weight-bold ls-25">How can I add new products?</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Venenatis tellus in metus vulputate eu
+                        scelerisquefelis. Vel pretium lectus quam id leo in vitae turpis massa. </p>
+                    <h4 className="font-weight-bold ls-25">How can I know stock?</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Venenatis tellus.</p>
+                </div>
+                <div className="col-md-6">
+                    <h4 className="font-weight-bold ls-25">How do I get paid?</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Venenatis tellus in metus vulputate eu
+                        scelerisquefelis. </p>
+                    <h4 className="font-weight-bold ls-25">Do I need a credit or debit card to create a shop?</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default QA;
