6c71f1c4871c10760e468c988b2c15807a3a165c
kaneki666 <msadman789@gmail.com>
2022-04-21 13:54:38 +0600

    21apr

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,21 @@
 import "../styles/globals.css";
 import { wrapper } from "../store/store";
-import { Provider, useStore } from "react-redux";
+import { useStore } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import "react-toastify/dist/ReactToastify.min.css";
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   const store = useStore((state) => state);
-  return (
+
+  return process.browser ? (
+    <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+      <Component {...pageProps} />
+    </PersistGate>
+  ) : (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
-  // return process.browser ? (
-  //   <QueryClientProvider client={queryClient}>
-  //     <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
-  //       <Component {...pageProps} />
-  //     </PersistGate>
-  //   </QueryClientProvider>
-  // ) : (
-  //   <QueryClientProvider client={queryClient}>
-  //     <PersistGate persistor={store}>
-  //       <Component {...pageProps} />
-  //     </PersistGate>
-  //   </QueryClientProvider>
-  // );
 }
 
 export default wrapper.withRedux(MyApp);
