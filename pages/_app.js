ec44e4ff760679e314754d15a2419bd10bb77008
kaneki666 <msadman789@gmail.com>
2022-04-21 13:56:20 +0600

    21apr

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { wrapper } from "../store/store";
-import { useStore } from "react-redux";
+import { Provider, useStore } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "react-toastify/dist/ReactToastify.min.css";
 
