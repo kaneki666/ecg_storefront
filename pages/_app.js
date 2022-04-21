1f64c49d32c3bc53b7682e46846d7873feefe706
kaneki666 <msadman789@gmail.com>
2022-04-21 13:45:25 +0600

    21apr

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { wrapper } from "../store/store";
-import { useStore } from "react-redux";
+import { Provider, useStore } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -8,20 +8,24 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   const store = useStore((state) => state);
-
-  return process.browser ? (
-    <QueryClientProvider client={queryClient}>
-      <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </QueryClientProvider>
-  ) : (
-    <QueryClientProvider client={queryClient}>
-      <PersistGate persistor={store}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </QueryClientProvider>
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   );
+  // return process.browser ? (
+  //   <QueryClientProvider client={queryClient}>
+  //     <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+  //       <Component {...pageProps} />
+  //     </PersistGate>
+  //   </QueryClientProvider>
+  // ) : (
+  //   <QueryClientProvider client={queryClient}>
+  //     <PersistGate persistor={store}>
+  //       <Component {...pageProps} />
+  //     </PersistGate>
+  //   </QueryClientProvider>
+  // );
 }
 
 export default wrapper.withRedux(MyApp);
